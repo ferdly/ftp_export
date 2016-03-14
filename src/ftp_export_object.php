33ab9c97aab5281b6f3c5a406cad122d9943cca1
ferdly <brad@qiqgroup.com>
2016-03-14 11:17:36 -0500

    to-from separated and to_from attribute added

--- src/ftp_export_object.php
+++ src/ftp_export_object.php
@@ -6,7 +6,30 @@ class ftpx_config {
   var $server_status = 'DEV';
   var $content_status = 'DEV';
   var $config_success = false;
+  var $archive_uri;
+  var $prune_email_count;
+  var $prune_email_array = array();
+  var $prune_alert_email_count;
+  var $prune_alert_email_array = array();
+
   public function __construct(){
+    /**
+     * PREFS
+     */
+    $client_contact = 'bradlowry+ftp_client@gmail.com';
+    $developer_contact = 'brad@qiqgroup.com';
+    $archive_uri = 'ftp_exportovernight/';
+    $prune_count = 360;// 2 files a day for ~ 6 months
+    $prune_alert_count = 720;// 2 files a day for ~ 12 months
+    $prune_email_array[] = $client_contact;
+    $prune_alert_email_array[] = $client_contact;
+    $prune_alert_email_array[] = $developer_contact;
+
+    $this->archive_uri = $archive_uri;
+    $this->prune_email_count = $prune_count;
+    $this->prune_alert_email_count = $prune_alert_count;
+    $this->prune_email_array = $prune_email_array;
+    $this->prune_alert_email_array = $prune_alert_email_array;
     return;
   }
   public function status_normalize()
@@ -59,6 +82,8 @@ class ftpx_instance {
   var $server_status = 'DEV';//or 'STAGING'/'TEST' or 'PROD'/'LIVE' is overloaded by parent
   var $content_status = 'DEV';//or 'STAGING'/'TEST' or 'PROD'/'LIVE' is overloaded by parent
   var $ftp_commonname;
+  var $ftp_combo_key_array = array();
+  var $ftp_to_from;
   var $ftp_host;
   var $ftp_username;
   var $ftp_password;
